9307195f8ac073d240c6fbe2634eae020d9bb188
jaya <jayashreehdj@gmai.com>
2024-05-31 12:24:08 +0530

    First push

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+function Home() {
+  return (
+    <div>
+      
+      <center><h1><img src='\shopify.jpg'></img></h1></center>
+      <center><h1>welcome</h1></center>
+
+    </div>
+  );
+}
+
+export default Home;
